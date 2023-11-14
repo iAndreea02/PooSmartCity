883037fd2d697032b0d3be021da71a5c1ae1a752
iAndreea02 <radu.andreea9@yahoo.com>
2023-11-14 20:48:27 +0200

    Smart_City - remove boolean bilet,methot intrare -add class Parc -remove from parameters nrAdulti,nrCopii -add Parc_Public ,menu -add par_public with activities -add: setnume -setAdresa -remove caruser -remove activitati and another method --add ArrayList

--- src/main/java/yas/proiect/smart/DemoTurism.java
+++ src/main/java/yas/proiect/smart/DemoTurism.java
@@ -5,6 +5,8 @@
  */
 package yas.proiect.smart;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author andre
@@ -12,8 +14,8 @@ package yas.proiect.smart;
 public class DemoTurism {
    
         public static void main(String[] args) {
-             int prog1[] = {7, 22}, prog2[] = {12, 18};
-    double[] pret1={5.6,10.3},pret0={6,11},pret3={0,6},pret4={10,27};
+        int[] prog1 = {7, 22}, prog2 = {12, 18},prog3={3,7},prog4={11,19},prog5={13,20};
+        double[] pret1={5.6,10.3},pret0={6,11},pret3={0,6},pret4={10,27};
         ParcPublic parc1 = new ParcPublic("Gradina Publica", "Strada Domneasca", prog1, 4.5f, pret0, 988.0,5,7,10);
         ParcPublic parc0 = new ParcPublic();
         ParcPublic parc_copy = new ParcPublic(parc1);
@@ -21,8 +23,12 @@ public class DemoTurism {
         Muzeu muzeu0 = new Muzeu();
         Muzeu muzeu_copy = new Muzeu(muzeu1);
         CasaMemoriala casa1 = new CasaMemoriala("Casa Cuza Voda", "Strada Alexandru Ioan Cuza", prog2,4.3f,pret3,"Casa", 1939, "Alexandru Ioan Cuza", "LA 158 DE ANI DE LA UNIREA PRINCIPATELOR ROMÂNE");
-       
-            System.out.println(parc1+"\n\n"+muzeu1+"\n\n"+casa1);
+        ArrayList<Muzeu> vecMuzeu=new ArrayList<Muzeu>();
+        vecMuzeu.add(new Muzeu());
+        vecMuzeu.add(new Muzeu("Muzeul din Gradina Botanica", "Faleza Galati", prog2, 4.9f, pret4, "Stiinte ale naturii", 1930,988));
+        
+        
+            System.out.println(vecMuzeu);
     }
         
 }
