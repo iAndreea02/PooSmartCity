26f4089a7480ecd70bd45fb7ffb59eb9570ec5de
Maria <78115465+MariaBlue13@users.noreply.github.com>
2023-12-11 23:24:37 +0200

    Update Spital.java

--- Spital.java
+++ Spital.java
@@ -1,28 +1,32 @@
 package ugal.ro.smartcity;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Spital extends Turism{
     public String tipServiciu;
     public String domeniu;
+    public String[] servicii;
    
     public Spital() 
     {
         super();
-        tipServiciu=" ";
         domeniu=" ";
+        servicii=new String[]{"Nimic"};
     }
 
-    public Spital(String tipServiciu, String domeniu, String nume, String adresa, int[] program, float rating, double[] pret)
+    public Spital(String nume, String adresa, int[] program, float rating, double[] pret, String domeniu, String[] servicii)
     {
         super(nume, adresa, program, rating, pret);
-        this.tipServiciu=tipServiciu;
+        this.domeniu=domeniu;
+        this.servicii=servicii;
     }
 
     public Spital(Spital copie)
     {
         super(copie);
-        tipServiciu=copie.tipServiciu;
+        domeniu=copie.domeniu;
+        servicii=copie.servicii;
     }
     
     public double preturi(String tipServiciu)
@@ -34,7 +38,7 @@ public class Spital extends Turism{
     m.put("ORL",pret[3]);
     m.put("Boli Infectioase",pret[4]);
     m.put("Radiologie",pret[5]);
-    m.put("Stomatologie",pret[6]);
+    m.put("Psihiatrie",pret[6]);
     m.put("Pediatrie",pret[7]);
     m.put("Cardiologie",pret[8]);
         return m.get(tipServiciu);
@@ -48,32 +52,32 @@ public class Spital extends Turism{
         {
             switch (tipServiciu)
             {
-                case "Neurologie" -> factura += pret[0];
-                case "Oftamologie" -> factura += pret[1];
-                case "Chirurgie" -> factura += pret[2];
-                case "ORL" -> factura += pret[3];
-                case "Boli Infectioase" -> factura += pret[4];
-                case "Radiologie" -> factura += pret[5];
-                case "Stomatologie" -> factura += pret[6];
-                case "Pediatrie" -> factura += pret[7];
-                case "Cardiologie" -> factura += pret[8];
+                case "Neurologie" -> bill += pret[0];
+                case "Oftamologie" -> bill += pret[1];
+                case "Chirurgie" -> bill += pret[2];
+                case "ORL" -> bill += pret[3];
+                case "Boli Infectioase" -> bill += pret[4];
+                case "Radiologie" -> bill += pret[5];
+                case "Stomatologie" -> bill += pret[6];
+                case "Pediatrie" -> bill += pret[7];
+                case "Cardiologie" -> bill += pret[8];
                 default -> System.out.println("Nu avem aceasta specialitate.");
             }
-            factura-=50;
+            bill-=50;
         }
         if(nrAdulti!=0)
         { 
             switch (tipServiciu)
             {
-                case "Neurologie" -> factura += pret[0];
-                case "Oftamologie" -> factura += pret[1];
-                case "Chirurgie" -> factura += pret[2];
-                case "ORL" -> factura += pret[3];
-                case "Boli Infectioase" -> factura += pret[4];
-                case "Radiologie" -> factura += pret[5];
-                case "Stomatologie" -> factura += pret[6];
-                case "Pediatrie" -> factura += pret[7];
-                case "Cardiologie" -> factura += pret[8];
+                case "Neurologie" -> bill += pret[0];
+                case "Oftamologie" -> bill += pret[1];
+                case "Chirurgie" -> bill += pret[2];
+                case "ORL" -> bill += pret[3];
+                case "Boli Infectioase" -> bill += pret[4];
+                case "Radiologie" -> bill += pret[5];
+                case "Stomatologie" -> bill += pret[6];
+                case "Pediatrie" -> bill += pret[7];
+                case "Cardiologie" -> bill += pret[8];
                 default -> System.out.println("Nu avem aceasta specialitate.");
             }
         }
@@ -91,12 +95,12 @@ public class Spital extends Turism{
         
         if("Da".equals(trimitereMedicala))
         {
-            factura=0.0;
+            bill=0.0;
             System.out.println("Srviciile din cadrul spitalelor sunt gratuite, pe baza trimiterii de la medicul de familie!");
         }
         else if("Nu".equals(trimitereMedicala))
                 {
-                    System.out.println("Plata servicilor este de "+factura+" lei.");
+                    System.out.println("Plata servicilor este de "+bill+" lei.");
                 }
         
     }
@@ -106,6 +110,13 @@ public class Spital extends Turism{
         public String toString()
     {
         return super.toString() + "\nTip serviciu: " + tipServiciu;
+    }
+        
+    public static void AfisareDomeniu(ArrayList<Spital> vectorSpital,String servicii,String domeniu)
+    {
+         for(Spital spital:vectorSpital)
+         {if(spital.domeniu.equals(domeniu) && spital.servicii.equals(servicii)) 
+             System.out.println(spital);}
     }
     
 }
