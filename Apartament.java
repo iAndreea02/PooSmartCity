cba10d27062279e7a137c0a9f100354320ef6379
Straciulita <151453316+Straciuita@users.noreply.github.com>
2024-01-12 19:13:12 +0200

    Add files via upload

--- Apartament.java
+++ Apartament.java
@@ -2,10 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Hotel;
-
+package proiect.lucru_cu_fisiere;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import static proiect.lucru_cu_fisiere.Hotel.Sterg; // AICI S AR PUTEA SA AI EROARE STERGI SI IMPORTI CEEA CE ITI ZICE DIN CLASA PARINTE HOTEL !!!!
 import yas.proiect.smart.Turism;
+
 
 public class Apartament extends Turism{
   
@@ -13,9 +24,10 @@ public class Apartament extends Turism{
     int nrlocuri;
     int nrApartament;
     int etaj;
+    
     boolean buc, micdejun, aerconditionat;
 
-
+//------------------------------------------------CONSTRUCTORI---------------------------------------------------
    public Apartament(){
         super();
         
@@ -28,7 +40,6 @@ public class Apartament extends Turism{
         micdejun=false;
             
     }
-
    public Apartament(Apartament c){
         super(c);
         
@@ -41,9 +52,8 @@ public class Apartament extends Turism{
         aerconditionat=c.aerconditionat;
         micdejun=c.micdejun;
         
-    }
-    
-   public Apartament(String nume,float rating, double pret[], String adresa,  int program[],String tippat,int nrlocuri, int nrcamere, int nrApartament, int etaj , boolean buc, boolean aerconditionat, boolean micdejun){
+    } 
+   public Apartament(String nume,float rating, double pret[], String adresa,  int program[],int nrlocuri, int nrcamere, int nrApartament, int etaj , boolean buc, boolean aerconditionat, boolean micdejun){
         super( nume,  adresa, program,  rating, pret);
         
         this.nrlocuri=nrlocuri;
@@ -55,7 +65,7 @@ public class Apartament extends Turism{
         this.aerconditionat=aerconditionat;
         this.micdejun=micdejun;
     }
-
+//-------------------------------------------------SUPRASCRIERE METODA TOSTRING------------------------------------------------------
     @Override
     public String toString() {
          return "Apartamentul cu numarul "+nrApartament+" are numele: "+nume+" are rating ul de "+rating+" stelute "+
@@ -64,9 +74,8 @@ public class Apartament extends Turism{
                "Se afla la etajul "+etaj+"\n"+
                "Are pretul "+pret[0]+ "lei \n";
     }
-    
+ //-------------------------------------------------------METODE----------------------------------------------------------------------   
    
-    
     public void afisareSpecificatii(){
        if(buc){
            System.out.println("Acest apartament are bucatarie.");
@@ -86,7 +95,6 @@ public class Apartament extends Turism{
            System.out.println("Daca stati cazati la acest apartament NU aveti mic de jun inclus in pret.");
        }
    }
-    
     public String Specificatii(){ // ACTUALIZAT PT INTERFATA
        StringBuilder result = new StringBuilder(); 
        if(buc){
@@ -107,74 +115,134 @@ public class Apartament extends Turism{
              result.append("Daca stati cazati la acest apartament NU aveti mic de jun inclus in pret.\n");
        }
         return result.toString();
-   }
-    
+   }  
     public int getNrcamere() {
         return nrcamere;
     }
-
     public void setNrcamere(int nrcamere) {
         this.nrcamere = nrcamere;
     }
-
     public int getNrlocuri() {
         return nrlocuri;
     }
-
     public void setNrlocuri(int nrlocuri) {
         this.nrlocuri = nrlocuri;
     }
-
     public int getNrApartament() {
         return nrApartament;
     }
-
     public void setNrApartament(int nrApartament) {
         this.nrApartament = nrApartament;
     }
-
     public int getEtaj() {
         return etaj;
     }
-
     public void setEtaj(int etaj) {
         this.etaj = etaj;
     }
-
     public boolean isBuc() {
         return buc;
     }
-
     public void setBuc(boolean buc) {
         this.buc = buc;
     }
-
     public boolean isMicdejun() {
         return micdejun;
     }
-
     public void setMicdejun(boolean micdejun) {
         this.micdejun = micdejun;
     }
-
     public boolean isAerconditionat() {
         return aerconditionat;
     }
-
     public void setAerconditionat(boolean aerconditionat) {
         this.aerconditionat = aerconditionat;
-    }
-   
-     public double factura(int nrnopti,int nrAdulti, int nrCopii){
+    } 
+    public double factura(int nrnopti,int nrAdulti, int nrCopii){
        return nrnopti*(pret[0]*nrAdulti+pret[0]/2*nrCopii);
     
-}
-     
+}   
     public static void AfisareNrloc(ArrayList<Apartament> vectorApartament, int nrloc, boolean aerconditionat) {
         for(Apartament apartament:vectorApartament){
          if(apartament.nrlocuri==nrloc && apartament.aerconditionat==aerconditionat) 
              System.out.println(apartament);
       }
     }
+    //------------------------------------------------------LUCRU CU FISIERE----------------------------------------------
+    public static void ScriereBinFis(ArrayList<Apartament> vectorApartament, File fisier){
+          try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(fisier))) {
+            outputStream.writeObject(vectorApartament);
+            System.out.println("Datele au fost scrise în fisier.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+     }
+    public static ArrayList CitireBinFis( File fisier) throws ClassNotFoundException{
+         ArrayList<Object> lista = null;
+         try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(fisier))) {
+            lista = (ArrayList<Object>) inputStream.readObject();
+             System.out.println("Datele au fost citite din fisier.");
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+          
+       return lista;
+          
+     }
+    //*********************************************************************************************************************
+    public static void ScriereTxtFis(ArrayList<Apartament> vectorApartament, File fisier){
+        
+        Sterg(fisier); //sterg ceea ce aveam inainte pt ca am dat true in parametrii pentru a putea scrie in continuare
+        for(int i=0;i<vectorApartament.size();i++){
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fisier, true))){
+            writer.write(vectorApartament.get(i).nume+"-"+vectorApartament.get(i).rating+ "-"+vectorApartament.get(i).pret[0]+"-"+vectorApartament.get(i).adresa+"-"+vectorApartament.get(i).program[0]+"-"+vectorApartament.get(i).program[1]+"-"+vectorApartament.get(i).nrlocuri+"-"+vectorApartament.get(i).nrcamere+"-"+vectorApartament.get(i).nrApartament+"-"+vectorApartament.get(i).etaj+"-"+vectorApartament.get(i).buc+"-"+vectorApartament.get(i).aerconditionat+"-"+vectorApartament.get(i).micdejun);
+            writer.write("\n");
+            
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        }
+        System.out.println("Am modificat continutul fisierului");
+    }
+    public static ArrayList CitireTxtFis(File fisier){
+         
+        ArrayList<Apartament> lista =  new ArrayList<>();;
+         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fisier))) {
+           
+            String linie;
+           
+            
+            while ((linie = bufferedReader.readLine()) != null) {
+                
+                String[] campuri = linie.split("-");
+                Apartament a = new Apartament(
+                        campuri[0], // Nume
+                        Float.parseFloat(campuri[1]), // Rating
+                        new double[]{ Double.parseDouble(campuri[2])}, // Pret
+                        campuri[3], // Adresa
+                        new int[]{Integer.parseInt(campuri[4]), Integer.parseInt(campuri[5])}, // prog
+                        Integer.parseInt(campuri[6]), // nrlocuri
+                        Integer.parseInt(campuri[7]), // nrcamere
+                        Integer.parseInt(campuri[8]), // nrapart
+                         Integer.parseInt(campuri[9]), //etaj
+                        Boolean.parseBoolean(campuri[10]), //buc
+                        Boolean.parseBoolean(campuri[11]), // aer
+                        Boolean.parseBoolean(campuri[12])  //mic dejun
+                        
+                );
 
+                
+                lista.add(a);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        
+       return lista;
+        
+        
+    }
 }
--- Hotel.java
+++ Hotel.java
@@ -2,20 +2,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Hotel;
+package proiect.lucru_cu_fisiere;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import java.util.Scanner;
 import yas.proiect.smart.Turism;
 import yas.proiect.smart.Turism;
 
-public class Hotel extends Turism{
+
+public class Hotel extends Turism implements Serializable{
    
    int nrcamere;
    boolean piscina, spa,restaurant, copii, animale;
    
    //---------------------------------------CONSTRUCTORI--------------------------------------------------------------------------------------------------------
-  public Hotel(){
+   public Hotel(){
        super();
        
        nrcamere=0;
@@ -25,9 +41,8 @@ public class Hotel extends Turism{
        copii=false;
        animale=false;
        
-   }
-   
-  public Hotel(Hotel c){
+   } 
+   public Hotel(Hotel c){
        
       super(c);
       
@@ -40,8 +55,7 @@ public class Hotel extends Turism{
      
               
    }
-   
-  public Hotel(String nume, float rating, double pret[], String adresa, int program[], int nrcamere, boolean piscina, boolean spa, boolean restaurant, boolean copii, boolean animale){
+   public Hotel(String nume, float rating, double pret[], String adresa, int program[], int nrcamere, boolean piscina, boolean spa, boolean restaurant, boolean copii, boolean animale){
       super( nume,  adresa, program,  rating, pret);
       this.nrcamere=nrcamere;
       this.piscina=piscina;
@@ -51,20 +65,16 @@ public class Hotel extends Turism{
       this.animale=animale;
        
    }
-  
    //---------------------------------------SUPRASCRIERE METODA TOSTRING-----------------------------------------------------------------------------------------
-  
    public String toString(){
        return "Hotelul "+ nume+" are "+rating+" stele este situat la adresa: "+adresa+ ".\n"+
               "Dispune de "+ nrcamere+" camere.\n "+
               "Se deschide la ora: "+ program[0]+" si se inchide la ora: "+program[1]+".\n"+
               "Pretul mediu este: "+pret[0]+" lei.\n";           
    }
-   
    //----------------------------------------METODE--------------------------------------------------------------------------------------------------------------
     
-   
-   public void afisareSpecificatii(){
+    public void afisareSpecificatii(){
        if(restaurant)
             System.out.println("Hotelul "+ nume+" are restaurant.");
        else System.out.println("Hotelul "+ nume+ " NU are restaurant.");
@@ -109,58 +119,46 @@ public class Hotel extends Turism{
 
         return result.toString();
    }
-   public double factura(int nrnopti,int nrAdulti, int nrCopii){
+    public double factura(int nrnopti,int nrAdulti, int nrCopii){
        return nrnopti*(pret[0]*nrAdulti+pret[0]/2*nrCopii);
    }
-
     public int getNrcamere() {
         return nrcamere;
     }
-
     public void setNrcamere(int nrcamere) {
         this.nrcamere = nrcamere;
     }
-
     public boolean isPiscina() {
         return piscina;
     }
-
     public void setPiscina(boolean piscina) {
         this.piscina = piscina;
     }
-
     public boolean isSpa() {
         return spa;
     }
-
     public void setSpa(boolean spa) {
         this.spa = spa;
     }
-
     public boolean isRestaurant() {
         return restaurant;
     }
-
     public void setRestaurant(boolean restaurant) {
         this.restaurant = restaurant;
     }
-
     public boolean isCopii() {
         return copii;
     }
-
     public void setCopii(boolean copii) {
         this.copii = copii;
     }
-
     public boolean isAnimale() {
         return animale;
     }
-
     public void setAnimale(boolean animale) {
         this.animale = animale;
     }
-     public static void AfisareRating (ArrayList<Hotel> vectorHotel,float rating, boolean piscina){
+    public static void AfisareRating (ArrayList<Hotel> vectorHotel,float rating, boolean piscina){
       
       
       for(Hotel hotel:vectorHotel){
@@ -169,4 +167,88 @@ public class Hotel extends Turism{
       }
       
   }
+   ///--------------------------------------------------------------------LUCRU CU FISIERE------------------------------------------------------------ 
+    public static void ScriereBinFis(ArrayList<Hotel> vectorHotel, File fisier){
+          try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(fisier))) {
+            outputStream.writeObject(vectorHotel);
+            System.out.println("Datele au fost scrise în fisier.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+     }
+    public static ArrayList CitireBinFis( File fisier) throws ClassNotFoundException{
+         ArrayList<Object> lista = null;
+         try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(fisier))) {
+            lista = (ArrayList<Object>) inputStream.readObject();
+             System.out.println("Datele au fost citite din fisier.");
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+          
+       return lista;
+          
+     }
+    //************************************************************************************************************************************************
+    public static void Sterg (File fisier){
+        try (FileWriter writer = new FileWriter(fisier, false)) {
+            writer.write("");
+            System.out.println("Continutul fisierului a fost sters.");        
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void ScriereTxtFis(ArrayList<Hotel> vectorHotel, File fisier){
+        
+        Sterg(fisier); //sterg ceea ce aveam inainte pt ca am dat true in parametrii pentru a putea scrie in continuare
+        for(int i=0;i<vectorHotel.size();i++){
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fisier, true))){
+            writer.write(vectorHotel.get(i).nume+"-"+vectorHotel.get(i).rating+ "-"+vectorHotel.get(i).pret[0]+"-"+vectorHotel.get(i).adresa+"-"+vectorHotel.get(i).program[0]+"-"+vectorHotel.get(i).program[1]+"-"+vectorHotel.get(i).nrcamere+"-"+vectorHotel.get(i).piscina+"-"+vectorHotel.get(i).spa+"-"+vectorHotel.get(i).restaurant+"-"+vectorHotel.get(i).animale+"-"+vectorHotel.get(i).copii);
+            writer.write("\n");
+            
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        }
+        System.out.println("Am modificat continutul fisierului");
+    }
+    public static ArrayList CitireTxtFis(File fisier){
+         
+        ArrayList<Hotel> lista =  new ArrayList<>();;
+         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fisier))) {
+           
+            String linie;
+           
+            
+            while ((linie = bufferedReader.readLine()) != null) {
+                
+                String[] campuri = linie.split("-");
+                Hotel hotel = new Hotel(
+                        campuri[0], // Nume
+                        Float.parseFloat(campuri[1]), // Rating
+                        new double[]{ Double.parseDouble(campuri[2])}, // Pret
+                        campuri[3], // Adresa
+                        new int[]{Integer.parseInt(campuri[4]), Integer.parseInt(campuri[5])}, // prog
+                        Integer.parseInt(campuri[6]), // camere
+                        Boolean.parseBoolean(campuri[7]), // Piscina
+                        Boolean.parseBoolean(campuri[8]), // spa
+                        Boolean.parseBoolean(campuri[9]), // restaurant
+                        Boolean.parseBoolean(campuri[10]), // copii
+                        Boolean.parseBoolean(campuri[11]) // animale
+                );
+
+                
+                lista.add(hotel);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        
+       return lista;
+        
+        
+    }
+    
 }
